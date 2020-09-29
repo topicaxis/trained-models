34682538fc4b4f8823df1bdbabd6c0bc086b2695
Matigakis Panagiotis <pmatigakis@gmail.com>
2020-09-29 21:41:14 +0300

    Bumped version to 0.6.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def get_requirements():
 
 setup(
     name='classifiers',
-    version='0.5.1',
+    version='0.6.0',
     packages=find_packages(exclude=["tests"]),
     include_package_data=True,
     zip_safe=False,
